2da434c58c5ef1eddc11e772903df47efcb0f33c
Connor Watson <connor.watson1997@gmail.com>
2024-08-28 19:34:26 +1000

    Section 4 Infrastucture Complete

---
+++ Restaurants.Infrastructure/Seeders/RestaurantSeeder.cs
@@ -0,0 +1,81 @@
+﻿using Restaurants.Domain.Entities;
+using Restaurants.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Infrastructure.Seeders
+{
+    internal class RestaurantSeeder(RestaurantDbContext dbContext) : IRestaurantSeeder
+    {
+        public async Task Seed()
+        {
+            if (await dbContext.Database.CanConnectAsync())
+            {
+                if (!dbContext.Restaurants.Any())
+                {
+                    var restaurants = GetRestaurants();
+                    dbContext.Restaurants.AddRange(restaurants);
+                    await dbContext.SaveChangesAsync();
+                }
+            }
+        }
+
+        private IEnumerable<Restaurant> GetRestaurants()
+        {
+            List<Restaurant> restaurants = [
+                new Restaurant()
+                {
+                    Name = "KFC",
+                    Category = "Fast Food",
+                    Description = "",
+                    ContactEmail = "hello@kfc.com",
+                    HasDelivery = true,
+                    Dishes = [
+                        new Dish()
+                        {
+                            Name = "Nashville Hot Chicken",
+                            Description = "",
+                            Price = 10.3M,
+                        },
+                        new Dish()
+                        {
+                            Name = "Chicken Nuggets",
+                            Description = "",
+                            Price = 5.3M,
+                        }
+                    ],
+                    Address = new Address()
+                    {
+                        City = "London",
+                        Street = "5 Cork Street",
+                        PostalCode = "W1F 8SR",
+
+                    }
+
+                },
+                new Restaurant()
+                {
+                    Name = "McDonald",
+                    Category = "Fast Food",
+                    Description = "",
+                    ContactEmail = "hello@maccas.com.au",
+                    HasDelivery = true,
+
+                    Address = new Address()
+                    {
+                        City = "London",
+                        Street = "Boots 193",
+                        PostalCode = "W1F 8SR",
+
+                    }
+                }
+            ];
+
+            return restaurants;
+
+        }
+    }
+}
