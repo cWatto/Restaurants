74d051237d5799aa666a9b6233d1881d78f30c08
Connor Watson <connor.watson1997@gmail.com>
2024-09-10 21:45:50 +1000

    Fixed bug

--- src/Restaurants.Application/Users/UserContext.cs
+++ src/Restaurants.Application/Users/UserContext.cs
@@ -16,7 +16,7 @@ public class UserContext
         var userId = user.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)!.Value;
         var email = user.FindFirst(c => c.Type == ClaimTypes.Email)!.Value;
         var roles = user.Claims.Where(c => c.Type == ClaimTypes.Role)!.Select(c => c.Value);
-        var nationality = user.FindFirst(c => c.Type == "Nationality")!.Value;
+        var nationality = user.FindFirst(c => c.Type == "Nationality")?.Value;
         var dateOfBirthString = user.FindFirst(c => c.Type == "DateOfBirth")?.Value;
 
         var dateOfBirth = dateOfBirthString == null ? (DateOnly?)null : DateOnly.ParseExact(dateOfBirthString, "yyyy-MM-dd");
